f11bca5f21e192cb834c8158a67de8eabd744696
Logan Langdon <logan.langdon@yahoo.com>
2023-08-11 21:07:42 -0700

    Added functionality to access list of all locations as a json representation.:

--- data_stores/data_stores.go
+++ data_stores/data_stores.go
@@ -3,6 +3,7 @@ package data_stores
 type MenuItem struct {
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
+  Ingredients []string `json:"ingredients`
 }
 
 type Location struct {
@@ -14,14 +15,28 @@ type Location struct {
 var Locations = []Location{
 	{
 		ID:   "01",
-		Name: "Boba Fiend",
+		Name: "Guppy House",
 		Menu: []MenuItem{
 			{
-				Name: "Green Milk Tea", Price: 3.50,
-			},
-			{
-				Name: "Passionfruit Green Tea", Price: 5.00,
-			},
+        Name: "Green Milk Tea", 
+        Price: 4.50, 
+        Ingredients: []string{"Non-dairy milk powder", "Jasmine Green Tea"},
+      },
+      {
+        Name: "Passionfruit Green Tea", 
+        Price: 5.00, 
+        Ingredients: []string{"Jasmine Green Tea", "Passionfruit Syrup", "High Fructose Corn Syrup"},
+      },
+      {
+        Name: "Brown Sugar Boba", 
+        Price: 0.50, 
+        Ingredients: []string{"Tapioca starch", "Brown Sugar"},
+      },
+      {
+        Name: "Lychee Jelly", 
+        Price: 0.50, 
+        Ingredients: []string{"Lychee fruit", "Gelatin"},
+      },
 		},
-	},
+  },
 }
--- server/server.go
+++ server/server.go
@@ -3,13 +3,30 @@ package server
 import (
 	"fmt"
 	"restfulBoba/data_stores"
-
+  "errors"
 	"github.com/gofiber/fiber/v2"
 )
 
 func getLocations(c *fiber.Ctx) error {
-	err := c.JSON(data_stores.Locations)
-	return err
+  err := c.JSON(data_stores.Locations)
+  return err
+}
+
+func getLocation(c *fiber.Ctx) error {
+  id := c.Params("id")
+  for i := 0; i < len(data_stores.Locations); i++ {
+    currStore := data_stores.Locations[i].ID
+    if currStore == id {
+      err := c.JSON(currStore)
+      return err
+    }
+  }
+  return errors.New(fmt.Sprintf("Location with id %s not found.",id))
+}
+
+func addMenuItem(c *fiber.Ctx) error {
+  var err error
+  return err
 }
 
 func LoadHandlers() {
@@ -26,17 +43,17 @@ func LoadHandlers() {
 		return c.SendString("Hello, World!")
 	})
 
-	if err := app.Get("/locations/:id", getLocations); err != nil {
-		fmt.Println("The error was totally handled, totally.")
-	}
-	app.Get("/:value", func(c *fiber.Ctx) error {
-		return c.SendString(c.Params("value") + " Bazoingas!?!!?!")
-	})
+	if err := app.Get("/locations", getLocations); err != nil {
+	  fmt.Println("Locations not found")
+  }
+  //	app.Get("/:value", func(c *fiber.Ctx) error {
+	//	return c.SendString(c.Params("value") + " Bazoingas!?!!?!")
+	//})
 
 	// GET http://localhost:8080/hello%20world
-	app.Get("/:name?", func(c *fiber.Ctx) error {
-		return c.SendString("Hello there, " + c.Params("name"))
-	})
+	//app.Get("/:name?", func(c *fiber.Ctx) error {
+	//	return c.SendString("Hello there, " + c.Params("name"))
+	//})
 
 	app.Listen(":3000")
 
